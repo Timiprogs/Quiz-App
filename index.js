e2274d533be369875ee24ef1451194c2473f2d6d
BANJO ENOCH <timi4rill@gmail.com>
2024-08-02 17:08:05 +0100

    latest update

--- index.js
+++ index.js
@@ -10,15 +10,6 @@ const questions=[
     ]
 },
 
-   { question:"When was the United Nations Organisation formed?",
-    answers:[
-        {text:"24th May 1933", correct:"false"},
-        {text:"19th March 1950", correct:"false"},
-        {text:"23rd October 1985", correct:"false"},
-        {text:"25th April 1945", correct:"true"}
-
-    ]
-},
 
  { question:"Who invented the light bulb?",
     answers:[
@@ -29,6 +20,7 @@ const questions=[
 
     ]
 },
+
    { question:" Who invented the motor car?",
     answers:[
         {text:"Henry Ford", correct:"false"},
@@ -89,6 +81,294 @@ const questions=[
 },
 
 
+    {
+        question: "When was the United Nations Organisation formed?",
+        answers: [
+            { text: "24th May 1933", correct: "false" },
+            { text: "19th March 1950", correct: "false" },
+            { text: "23rd October 1985", correct: "false" },
+            { text: "25th April 1945", correct: "true" }
+        ]
+    },
+    {
+        question: "Who is the author of '1984'?",
+        answers: [
+            { text: "George Orwell", correct: "true" },
+            { text: "Aldous Huxley", correct: "false" },
+            { text: "Ray Bradbury", correct: "false" },
+            { text: "F. Scott Fitzgerald", correct: "false" }
+        ]
+    },
+    {
+        question: "What is the chemical symbol for water?",
+        answers: [
+            { text: "H2O2", correct: "false" },
+            { text: "CO2", correct: "false" },
+            { text: "H2O", correct: "true" },
+            { text: "O2", correct: "false" }
+        ]
+    },
+    {
+        question: "Who developed the theory of relativity?",
+        answers: [
+            { text: "Isaac Newton", correct: "false" },
+            { text: "Albert Einstein", correct: "true" },
+            { text: "Stephen Hawking", correct: "false" },
+            { text: "Niels Bohr", correct: "false" }
+        ]
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        answers: [
+            { text: "Jupiter", correct: "false" },
+            { text: "Mars", correct: "true" },
+            { text: "Saturn", correct: "false" },
+            { text: "Neptune", correct: "false" }
+        ]
+    },
+    {
+        question: "What is the largest organ in the human body?",
+        answers: [
+            { text: "Brain", correct: "false" },
+            { text: "Heart", correct: "false" },
+            { text: "Liver", correct: "false" },
+            { text: "Skin", correct: "true" }
+        ]
+    },
+    {
+        question: "Which country is the largest by land area?",
+        answers: [
+            { text: "Canada", correct: "false" },
+             { text: "Russia", correct: "true" },
+            { text: "China", correct: "false" },
+            { text: "United States", correct: "false" }
+        ]
+    },
+    {
+        question: "Who painted the 'Starry Night'?",
+        answers: [
+       
+            { text: "Pablo Picasso", correct: "false" },
+            { text: "Claude Monet", correct: "false" },
+            { text: "Leonardo da Vinci", correct: "false" },
+            { text: "Vincent van Gogh", correct: "true" },
+        ]
+    },
+    {
+    question: "Which country won the FIFA World Cup in 2018?",
+    answers: [
+        { text: "Germany", correct: "false" },
+        { text: "Brazil", correct: "false" },
+        { text: "France", correct: "true" },
+        { text: "Argentina", correct: "false" }
+    ]
+},
+{
+    question: "Who holds the record for the most Olympic gold medals?",
+    answers: [
+        { text: "Michael Phelps", correct: "true" },
+        { text: "Usain Bolt", correct: "false" },
+        { text: "Serena Williams", correct: "false" },
+        { text: "Simone Biles", correct: "false" }
+    ]
+},
+{
+    question: "Which sport is known as 'The Sport of Kings'?",
+    answers: [
+        { text: "Tennis", correct: "false" },
+        { text: "Polo", correct: "true" },
+        { text: "Horse Racing", correct: "false" },
+        { text: "Golf", correct: "false" }
+    ]
+},
+{
+    question: "Which NBA player scored 81 points in a single game?",
+    answers: [
+        { text: "LeBron James", correct: "false" },
+        { text: "Kobe Bryant", correct: "true" },
+        { text: "Kevin Durant", correct: "false" },
+        { text: "Stephen Curry", correct: "false" }
+    ]
+},
+{
+    question: "In which city are the headquarters of the International Olympic Committee (IOC) located?",
+    answers: [
+        { text: "Lausanne", correct: "true" },
+        { text: "Geneva", correct: "false" },
+        { text: "Paris", correct: "false" },
+        { text: "New York", correct: "false" }
+    ]
+},{
+    question: "Which tennis player has won the most Grand Slam singles titles?",
+    answers: [
+        { text: "Rafael Nadal", correct: "false" },
+        { text: "Serena Williams", correct: "true" },
+        { text: "Roger Federer", correct: "false" },
+        { text: "Novak Djokovic", correct: "false" }
+    ]
+},
+{
+    question: "Which team has won the most Super Bowl championships in NFL history?",
+    answers: [
+        { text: "Pittsburgh Steelers", correct: "false" },
+        { text: "San Francisco 49ers", correct: "false" },
+        { text: "New England Patriots", correct: "true" },
+        { text: "Dallas Cowboys", correct: "false" }
+    ]
+},
+
+
+    {
+        question: "Which is the largest mammal on Earth?",
+        answers: [
+            { text: "Blue whale", correct: "true" },
+            { text: "African elephant", correct: "false" },
+            { text: "Giraffe", correct: "false" },
+            { text: "Hippopotamus", correct: "false" }
+        ]
+    },
+    {
+        question: "Who discovered penicillin?",
+        answers: [
+            { text: "Alexander Fleming", correct: "true" },
+            { text: "Marie Curie", correct: "false" },
+            { text: "Louis Pasteur", correct: "false" },
+            { text: "Thomas Edison", correct: "false" }
+        ]
+    },
+    {
+        question: "Which bird is known for its ability to mimic human speech?",
+        answers: [
+       
+            { text: "Crow", correct: "false" },
+            { text: "Pigeon", correct: "false" },
+            { text: "Parrot", correct: "true" },
+            { text: "Owl", correct: "false" }
+        ]
+    },
+    {
+        question: "What is the largest ocean on Earth?",
+        answers: [
+         
+            { text: "Indian Ocean", correct: "false" },
+            { text: "Atlantic Ocean", correct: "false" },
+            { text: "Arctic Ocean", correct: "false" },
+            { text: "Pacific Ocean", correct: "true" },
+        ]
+    },
+    {
+        question: "Who wrote 'The Great Gatsby'?",
+        answers: [
+            { text: "F. Scott Fitzgerald", correct: "true" },
+            { text: "Ernest Hemingway", correct: "false" },
+            { text: "John Steinbeck", correct: "false" },
+            { text: "J.D. Salinger", correct: "false" }
+        ]
+    },
+    {
+        question: "What is the currency of Japan?",
+        answers: [
+           
+            { text: "Dollar", correct: "false" },
+            { text: "Euro", correct: "false" },
+             { text: "Yen", correct: "true" },
+            { text: "Pound", correct: "false" }
+        ]
+    },
+    {
+        question: "Who invented the telephone?",
+        answers: [
+            { text: "Alexander Graham Bell", correct: "true" },
+            { text: "Thomas Edison", correct: "false" },
+            { text: "Nikola Tesla", correct: "false" },
+            { text: "Guglielmo Marconi", correct: "false" }
+        ]
+    },
+    {
+        question: "Which planet is known as the 'Morning Star' or 'Evening Star'?",
+        answers: [
+            { text: "Mars", correct: "false" },
+            { text: "Venus", correct: "true" },
+            { text: "Mercury", correct: "false" },
+            { text: "Jupiter", correct: "false" }
+        ]
+    },
+    {
+        question: "What is the chemical symbol for gold?",
+        answers: [
+        
+            { text: "Ag", correct: "false" },
+            { text: "Cu", correct: "false" },
+            { text: "Fe", correct: "false" },
+            { text: "Au", correct: "true" },
+        ]
+    },
+    {
+        question: "Who is known as the 'Father of Modern Physics'?",
+        answers: [
+            { text: "Albert Einstein", correct: "true" },
+            { text: "Isaac Newton", correct: "false" },
+            { text: "Niels Bohr", correct: "false" },
+            { text: "Galileo Galilei", correct: "false" }
+        ]
+    },
+    {
+        question: "Which instrument is used to measure atmospheric pressure?",
+        answers: [
+          
+            { text: "Thermometer", correct: "false" },
+            { text: "Hygrometer", correct: "false" },
+            { text: "Anemometer", correct: "false" },
+            { text: "Barometer", correct: "true" },
+        ]
+    },
+    {
+        question: "Who was the first woman to fly solo across the Atlantic Ocean?",
+        answers: [
+            
+            { text: "Valentina Tereshkova", correct: "false" },
+            { text: "Amelia Earhart", correct: "true" },
+            { text: "Sally Ride", correct: "false" },
+            { text: "Bessie Coleman", correct: "false" }
+        ]
+    },
+    {
+        question: "Which animal is known as the 'King of the Jungle'?",
+        answers: [
+            
+            { text: "Tiger", correct: "false" },
+            { text: "Leopard", correct: "false" },
+            { text: "Lion", correct: "true" },
+            { text: "Cheetah", correct: "false" }
+        ]
+    },
+    {
+        question: "Who invented the light bulb?",
+        answers: [
+            { text: "Thomas Edison", correct: "true" },
+            { text: "Alexander Graham Bell", correct: "false" },
+            { text: "Nikola Tesla", correct: "false" },
+            { text: "Galileo Galilei", correct: "false" }
+        ]
+    },
+    {
+        question: "What is the capital city of Australia?",
+        answers: [
+            { text: "Sydney", correct: "false" },
+            { text: "Melbourne", correct: "false" },
+            { text: "Canberra", correct: "true" },
+            { text: "Brisbane", correct: "false" }
+        ]
+    },
+    {
+        question: "Who wrote 'The Catcher in the Rye'?",
+        answers: [
+            { text: "J.D. Salinger", correct: "true" },
+            { text: "F. Scott Fitzgerald", correct: "false" },
+            { text: "Ernest Hemingway", correct: "false" },
+            { text: "John Steinbeck", correct: "false" }
+        ]
+    },
  { question:" When did the first world war take place?",
     answers:[
         {text:"1912-1916", correct:"false"},
